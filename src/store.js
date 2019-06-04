9a11139565285f5a1636833c490acbfb4aacd6b5
dszocik <damian.szocik@hrtec.pl>
2019-06-04 16:42:46 +0200

    cleaned up redux store initialisation

--- src/store.js
+++ src/store.js
@@ -3,28 +3,7 @@ import {
     applyMiddleware,
     compose
 } from 'redux';
-
 import ReduxThunk from 'redux-thunk';
-
-
-import {
-    loadStore,
-    saveStore
-} from './store/localStorage'
-
-import {
-    firebaseRef
-} from './store/firebase'
-
 import reducers from './reducers'
 
-const savedStore = loadStore();
-if (savedStore) {
-    // firebaseRef.set(savedStore);
-}
-export let store = createStore(reducers, {}, compose(applyMiddleware(ReduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-// store.subscribe(() => {
-// saveStore(store.getState());
-// firebase don't accept undefined properties - stringify and then parse to cut off all undefined properties
-// firebaseRef.set(JSON.parse(JSON.stringify(store.getState())));
-// })
+export let store = createStore(reducers, {}, compose(applyMiddleware(ReduxThunk) /* , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */ ));
