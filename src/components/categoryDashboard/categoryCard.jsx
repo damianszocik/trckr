d58e7fef51b1d1f47f4233126be81a572df45823
damianszocik <damian.szocik@gmail.com>
2019-09-02 21:33:15 +0200

    category card redesign

--- src/components/categoryDashboard/categoryCard.jsx
+++ src/components/categoryDashboard/categoryCard.jsx
@@ -1,65 +1,82 @@
 import React from 'react';
-import { Card, Row, Col, Icon } from 'antd';
+import { Card, Row, Col, Icon, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 
 const countValues = category => {
- let trackers = 0;
- let categories = 0;
+    let trackers = 0;
+    let categories = 0;
 
- for (let el in category.data) {
-  if (category.data[el].type == 'category') {
-   categories++;
-  } else if (category.data[el].type == 'tracker') {
-   trackers++;
-  }
- }
- return { trackers, categories };
+    for (let el in category.data) {
+        if (category.data[el].type == 'category') {
+            categories++;
+        } else if (category.data[el].type == 'tracker') {
+            trackers++;
+        }
+    }
+    return { trackers, categories };
 };
 
+const CardHeader = () => {
+    return (
+        <Row>
+            <Col>
+                <Typography.Text type="secondary">
+                    <Icon type="folder" /> Category
+                </Typography.Text>
+            </Col>
+        </Row>
+    )
+}
+
 export default function CategoryCard(props) {
- return (
-  <Card
-   className="mt-4 flex flex-column"
-   title={
-    <span>
-     <Icon type="folder" /> {props.category.name}
-    </span>
-   }
-   extra={
-    <Link to={`/category/${props.category.id}`}>
-     <Icon type="arrow-right" />
-    </Link>
-   }
-   style={{ width: '100%' }}
-   bodyStyle={{ flexGrow: 1 }}>
-   <Row type="flex" align="middle" className="height-100" justify="space-around">
-    <Col lg={24} xl={12}>
-     <Row type="flex" justify="center">
-      <Col>
-       <Icon style={{ fontSize: '4em' }} className="mb-1" type="folder" />
-      </Col>
-     </Row>
-     <Row type="flex" justify="center">
-      <Col>
-       <p className="width-100 text-center font-bold font-size-150 my-0">{countValues(props.category).categories}</p>
-       <p className="width-100 text-center my-1">Categories</p>
-      </Col>
-     </Row>
-    </Col>
-    <Col lg={24} xl={12}>
-     <Row type="flex" justify="center">
-      <Col>
-       <Icon style={{ fontSize: '4em' }} className="mb-1" type="stock" />
-      </Col>
-     </Row>
-     <Row type="flex" justify="center">
-      <Col>
-       <p className="width-100 text-center font-bold font-size-150 my-0">{countValues(props.category).trackers}</p>
-       <p className="text-center my-1">Trackers</p>
-      </Col>
-     </Row>
-    </Col>
-   </Row>
-  </Card>
- );
+    return (
+        <Card
+            className="mt-4 flex flex-column"
+            title={<CardHeader />}
+            style={{ width: '100%' }}
+            bodyStyle={{ flexGrow: 1 }}>
+            <div className="height-100 flex flex-column flex-justify-between">
+                <Row className="mb-4">
+                    <Col>
+                        <Typography.Title className="m-0" level={4}>{props.category.name}</Typography.Title>
+                    </Col>
+                </Row>
+                {Object.keys(props.category.data).length ? (
+                    <React.Fragment>
+                        <Row>
+                            <Col>
+                                <Typography.Text>
+                                    <Icon type="folder" className="mr-1" />
+                                    {countValues(props.category).categories} {countValues(props.category).categories === 1 ? 'category' : 'categories'}
+                                </Typography.Text>
+                            </Col>
+                        </Row>
+                        <Row className="mt-1">
+                            <Col>
+                                <Typography.Text>
+                                    <Icon type="stock" className="mr-1" />
+                                    {countValues(props.category).trackers} {countValues(props.category).trackers === 1 ? 'tracker' : 'trackers'}
+                                </Typography.Text>
+                            </Col>
+                        </Row>
+                    </React.Fragment>) : (
+                        <Row className="my-2">
+                            <Col>
+                                <Typography.Text>
+                                    <Icon type="info-circle" className="mr-1" />Empty category
+               </Typography.Text>
+                            </Col>
+                        </Row>
+                    )}
+                <Row className="mt-4">
+                    <Col>
+                        <Link to={`/category/${props.category.id}`}>
+                            Open
+                   <Icon type="arrow-right" className="ml-1" />
+                        </Link>
+                    </Col>
+                </Row>
+            </div>
+        </Card>
+    );
 }
